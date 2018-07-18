77b5c6db659db02065e9315cd2af0a4028fe94c5
raji <diyaraji@gmail.com>
2018-07-18 17:54:32 +0530

    tried adding enquiry,but dont know

--- studyspace/app1/views.py
+++ studyspace/app1/views.py
@@ -6,6 +6,11 @@ from django.http import HttpResponse
 from app1.models import StudyHall,Expenses,Enquiry
 # Create your views here.
 def viewindex(request):
+	if request.method == "POST":
+		#import pdb;pdb.set_trace()
+		data=request.POST
+		hall=StudyHall(name=data.get("hall_name"),area=data.get("hall_area"))
+		hall.save()
 	studyhalls = StudyHall.objects.all()
 	exp = Expenses.objects.all()
 	enq = Enquiry.objects.all()
