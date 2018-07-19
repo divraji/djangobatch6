626279284ed5c30369614ddb1c66fd9d6f6e5106
raji <diyaraji@gmail.com>
2018-07-19 17:51:24 +0530

    Bootstrap style for StudyHall,Enquiry,Expense

--- studyspace/app1/views.py
+++ studyspace/app1/views.py
@@ -3,21 +3,44 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
-from app1.models import StudyHall,Expenses,Enquiry
+from app1.models import StudyHall,Expenses,Enquiry,Course,Student
 # Create your views here.
 def viewindex(request):
 	if request.method == "POST":
 		#import pdb;pdb.set_trace()
 		data=request.POST
-		hall=StudyHall(name=data.get("hall_name"),area=data.get("hall_area"))
-		hall.save()
+		if data.get("studyhall"):
+			hall=StudyHall(name=data.get("hall_name"),area=data.get("hall_area"))
+			hall.save()
+		elif data.get("enquiry"):
+			name = data.get("enq_name")
+			course = data.get("enq_course")
+			student = data.get("enq_student")
+			course_inst = Course.objects.get(pk=course)
+			student_inst = Student.objects.get(pk=student)
+			enq_inst = Enquiry(name=name, course=course_inst,student = student_inst)
+			enq_inst.save()
+		elif data.get("expense"):
+			studyhall_inst = StudyHall.objects.get(pk=data.get("exp_studyhall"))
+			exp = Expenses(
+				studyhall = studyhall_inst,
+				date = data.get("exp_date"),
+				name = data.get("exp_name"),
+				desc = data.get("exp_desc"),
+				value = data.get("exp_value"),
+				)
+			exp.save()
 	studyhalls = StudyHall.objects.all()
 	exp = Expenses.objects.all()
 	enq = Enquiry.objects.all()
+	cour = Course.objects.all()
+	stud = Student.objects.all()
 
 	return render(request,"app1/index.html" ,{
 		"datahalls": studyhalls,
 		"dataexps": exp,
-		"dataenqs": enq
+		"dataenqs": enq,
+		"datacourse":cour,
+		"datastudent":stud
 		})
 
